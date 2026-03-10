390d3be25a0fb2e3836d5424eff47843c2ab95c9
luna <amandaluna.castro1602@gmail.com>
2026-03-09 21:34:39 -0300

    fix update

---
+++ scripts/validate.js
@@ -0,0 +1,66 @@
+// Função para mostrar erro
+function showInputError(formElement, inputElement, errorMessage) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add("popup__form__input_type_error");
+  errorElement.textContent = errorMessage;
+}
+
+// Função para esconder erro
+function hideInputError(formElement, inputElement) {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove("popup__form__input_type_error");
+  errorElement.textContent = "";
+}
+
+// Função para verificar validade
+function checkInputValidity(formElement, inputElement) {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+}
+
+// Função para controlar botão
+function toggleButtonState(inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add("popup__form__submit_disabled");
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove("popup__form__submit_disabled");
+    buttonElement.disabled = false;
+  }
+}
+
+// Função para verificar se há input inválido
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+
+function setEventListeners(formElement) {
+  const inputList = Array.from(
+    formElement.querySelectorAll(".popup__form__input"),
+  );
+  const buttonElement = formElement.querySelector(".popup__form__submit");
+
+  // Desabilita o botão inicialmente
+  toggleButtonState(inputList, buttonElement);
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+  });
+}
+
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll(".popup__form"));
+  formList.forEach((formElement) => {
+    setEventListeners(formElement);
+  });
+}
+
+enableValidation();
